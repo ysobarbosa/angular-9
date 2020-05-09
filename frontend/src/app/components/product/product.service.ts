9509e97c86b664a3cb778eb8eead5c5b2ead044e
Yasmin <ysobarbosa@gmail.com>
2020-05-09 15:51:08 -0300

    metade do curso

---
+++ frontend/src/app/components/product/product.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor() { }
+
+  showOnConsole(msg: string): void {
+    console.log(msg);
+  }
+}
